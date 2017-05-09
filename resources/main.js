6dd6d2f81b0240f62190a226c6a032c605f7e5fc
Vultariev <Vultariev@users.noreply.github.com>
2017-05-09 15:17:19 -0400

    Update main.js

--- resources/main.js
+++ resources/main.js
@@ -1,4 +1,4 @@
-var canvas = document.getElementById("theCanvas");
+      var canvas = document.getElementById("theCanvas");
       var ctx = canvas.getContext("2d");
       function sleep(ms) {
         return new Promise(resolve => setTimeout(resolve, ms));
