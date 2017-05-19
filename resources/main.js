e17e1959e1a8a3818a94fa5d997c28a92426491a
Vultariev <vultariev@gmail.com>
2017-05-19 13:38:38 -0400

    Finished addPlayer()

--- resources/main.js
+++ resources/main.js
@@ -67,7 +67,7 @@ function player(name,piece)
 }
 function addPlayer()
 {
-  
+  players[players.length] = new player(document.getElementById("playername")+"".value,document.getElementByName("piece").value+"");
 }
 function main(stat)
 {
